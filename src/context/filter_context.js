2916a0b412ee95410a77ccb83c4cb8bcb28510a9
unknown <krsmanovic013@gmail.com>
2023-05-18 19:36:33 +0200

    all sorts done

---
+++ src/context/filter_context.js
@@ -0,0 +1,59 @@
+import { useEffect, useContext, useReducer, createContext } from "react";
+import reducer from "../reducers/filter_reducer";
+import {
+  LOAD_PRODUCTS,
+  SET_GRIDVIEW,
+  SET_LISTVIEW,
+  UPDATE_SORT,
+  SORT_PRODUCTS,
+  UPDATE_FILTERS,
+  FILTER_PRODUCTS,
+  CLEAR_FILTERS,
+} from "../actions";
+import { useProductContext } from "./products_context";
+
+const initialState = {
+  filtered_products: [],
+  all_products: [],
+  grid_view: true,
+  sort: "price-lowest",
+};
+
+const FilterContext = createContext();
+
+export const useFilterContext = () => useContext(FilterContext);
+
+export const FilterProvider = ({ children }) => {
+  const { products } = useProductContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const setGridView = () => {
+    dispatch({ type: SET_GRIDVIEW });
+  };
+
+  const setListView = () => {
+    dispatch({ type: SET_LISTVIEW });
+  };
+
+  const updateSort = (e) => {
+    // const name = e.target.name;
+    const value = e.target.value;
+    dispatch({ type: UPDATE_SORT, payload: value });
+  };
+
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products });
+  }, [products]);
+
+  useEffect(() => {
+    dispatch({ type: SORT_PRODUCTS });
+  }, [products, state.sort]);
+
+  return (
+    <FilterContext.Provider
+      value={{ ...state, setGridView, setListView, updateSort }}
+    >
+      {children}
+    </FilterContext.Provider>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ProductsProvider>
-    <App />
+    <FilterProvider>
+      <App />
+    </FilterProvider>
   </ProductsProvider>
 );
